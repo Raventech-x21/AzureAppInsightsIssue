e68604548260c4e59c630587467c7fbb0e80a884
Michael Copley <copley_michael@hotmail.com>
2024-01-05 09:21:47 +0800

    Changes to undo default behavour of ConfigureFunctionsApplicationInsights

--- TimerFunctionApp/TimerFunctionApp/Program.cs
+++ TimerFunctionApp/TimerFunctionApp/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
-        //services.ConfigureFunctionsApplicationInsights(); // This stops all 'information' level log messages from being sent to App Insights (for some unknown reason)
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .ConfigureLogging(logging =>
+    {
+        // https://learn.microsoft.com/en-au/azure/azure-functions/dotnet-isolated-process-guide#application-insights
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (defaultRule != null)
+                options.Rules.Remove(defaultRule);
+        });
     })
     .Build();
 
