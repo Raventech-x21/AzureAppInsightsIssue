b75ac3dc96880b9c6dd0d8c3ceed2db97cf5df6d
Michael Copley <copley_michael@hotmail.com>
2024-01-05 09:24:08 +0800

    Reverted to standard .NET 7.0 template for logging

--- TimerFunctionApp/TimerFunctionApp/Program.cs
+++ TimerFunctionApp/TimerFunctionApp/Program.cs
@@ -1,26 +1,30 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
+// .NET 7.0 template implementation
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-    })
-    .ConfigureLogging(logging =>
-    {
-        // https://learn.microsoft.com/en-au/azure/azure-functions/dotnet-isolated-process-guide#application-insights
-        logging.Services.Configure<LoggerFilterOptions>(options =>
-        {
-            var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-
-            if (defaultRule != null)
-                options.Rules.Remove(defaultRule);
-        });
-    })
+    .ConfigureServices(services => { })
     .Build();
+
+// .NET 8.0 template implementation
+//var host = new HostBuilder()
+//    .ConfigureFunctionsWorkerDefaults()
+//    .ConfigureServices(services =>
+//    {
+//        services.AddApplicationInsightsTelemetryWorkerService();
+//        services.ConfigureFunctionsApplicationInsights();
+//    })
+//    .ConfigureLogging(logging =>
+//    {
+//        // https://learn.microsoft.com/en-au/azure/azure-functions/dotnet-isolated-process-guide#application-insights
+//        logging.Services.Configure<LoggerFilterOptions>(options =>
+//        {
+//            var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+//            if (defaultRule != null)
+//                options.Rules.Remove(defaultRule);
+//        });
+//    })
+//    .Build();
 
 host.Run();
