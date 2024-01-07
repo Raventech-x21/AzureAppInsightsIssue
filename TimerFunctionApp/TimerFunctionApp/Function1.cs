96333871d98c3984faa92670fb1af40c0561f04d
Michael Copley <copley_michael@hotmail.com>
2024-01-07 10:47:59 +0800

    Changes to check the number of log messages in the LAW and alert on mismatch

--- TimerFunctionApp/TimerFunctionApp/Function1.cs
+++ TimerFunctionApp/TimerFunctionApp/Function1.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using Azure.Monitor.Query;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -6,6 +8,7 @@ namespace TimerFunctionApp
     public class Function1
     {
         private readonly ILogger _logger;
+        private const int NumberOfMessages = 1000;
 
         public Function1(ILoggerFactory loggerFactory)
         {
@@ -13,7 +16,7 @@ namespace TimerFunctionApp
         }
 
         [Function("Function1")]
-        public void Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 0 * * * *")] TimerInfo myTimer)
         {
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
@@ -24,12 +27,35 @@ namespace TimerFunctionApp
 
             var correlationId = Guid.NewGuid();
 
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= NumberOfMessages; i++)
             {
                 _logger.LogInformation($"[{correlationId}] Logging information message #{i}");
                 _logger.LogWarning($"[{correlationId}] Logging warning message #{i}");
                 _logger.LogError($"[{correlationId}] Logging error message #{i}");
             }
+
+            _logger.LogInformation($"Waiting ten minutes before checking logs...");
+
+            await Task.Delay(TimeSpan.FromMinutes(10));
+
+            _logger.LogInformation($"Checking logs...");
+
+            var workspaceId = "363c3473-7f57-4605-be76-f2b58605c2ca";
+            var credential = new DefaultAzureCredential();
+            var client = new LogsQueryClient(credential);
+            var timeRange = new QueryTimeRange(TimeSpan.FromHours(1));
+
+            var kqlQuery = $"AppTraces | where OperationName == 'Function1' and Message startswith '[{correlationId}]' and SeverityLevel == 1 | count";
+
+            var response = await client.QueryWorkspaceAsync(workspaceId, kqlQuery, timeRange);
+
+            var messageCount = int.Parse(response.Value.Table.Rows[0][0].ToString());
+
+            _logger.LogInformation($"Count of messages: {messageCount}");
+
+            // TODO : change this back to != NumberOfMessages when i've tested the alert
+            if (messageCount != 500)
+                _logger.LogCritical($"Did not log {NumberOfMessages} messages");
         }
     }
 }
